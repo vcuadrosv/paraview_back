5c10266eb25a0691ea36bac079c6094527cff1d9
vcuadrosv <valery.05@hotmail.com>
2025-05-20 19:45:30 -0500

    feat

--- app_ec2.py
+++ app_ec2.py
@@ -54,9 +54,9 @@ if os.path.exists(x_lock):
 # Comando para ejecutar ParaViewWeb Visualizer
 command = [
     "xvfb-run", "--auto-servernum","--server-num=99","-s", "-screen 0 1024x768x24",
-    "~/ParaView-5.11.0-MPI-Linux-Python3.9-x86_64/bin/pvpython",
-    "~/paraviewweb-visualizer/server/pvw-visualizer.py",
-    "--content", "~/paraviewweb-visualizer/dist",
+    "/home/ubuntu/ParaView-5.11.0-MPI-Linux-Python3.9-x86_64/bin/pvpython",
+    "/home/ubuntu/paraviewweb-visualizer/server/pvw-visualizer.py",
+    "--content", "/home/ubuntu/paraviewweb-visualizer/dist",
     "--port", "1234",
     "--data", vtk_dir,
     "--host", "0.0.0.0"
